03889f9a705b3ded69619dca34df3e0e31529d9a
Jonathan <30727873+ea1het@users.noreply.github.com>
2021-12-19 07:52:01 +0100

    Update tpl_logger.py

--- tpl_logger.py
+++ tpl_logger.py
@@ -2,13 +2,26 @@
 # pylint: disable=locally-disabled, multiple-statements
 # pylint: disable=fixme, line-too-long, invalid-name
 # pylint: disable=wrong-spelling-in-comment
-# pylint: disable=W0703
+# pylint: disable=broad-except
+
+################################################################################
+#
+# Log statements in your code:
+# - logger.debug('this is a debug statement')
+# - logger.info('this is a info statement')
+# - logger.warning('this is a warning statement')
+# - logger.critical('this is a critical statement')
+# - logger.error('this is a error statement')
+#
+# Logger also accepts modifiers for extra information in logs (traceback):
+# - logger.error("Error happened!", exc_info=True)
+#
+################################################################################
 
 """ ${PROJECT_NAME} """
 
 __author__ = '${USER}'
 __date__ = '${DATE}-${TIME}'
-
 
 import logging
 
@@ -35,13 +48,3 @@ file_handler.setFormatter(file_formatter)
 
 logger.addHandler(shell_handler)
 logger.addHandler(file_handler)
-
-# Log statements in your code:
-# - logger.debug('this is a debug statement')
-# - logger.info('this is a info statement')
-# - logger.warning('this is a warning statement')
-# - logger.critical('this is a critical statement')
-# - logger.error('this is a error statement')
-#
-# Logger also accpets modifiers for extra information in logs (traceback):
-# - logger.error("Error happened!", exc_info=True)
